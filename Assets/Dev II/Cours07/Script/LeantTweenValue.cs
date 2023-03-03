d5421e45120d73437ca9c71b792beb20667bbf8e
Humarcoux <humarcoux@osullivan-quebec.qc.ca>
2023-03-03 09:35:06 -0500

    bidon

--- Assets/Dev II/Cours07/Script/LeantTweenValue.cs
+++ Assets/Dev II/Cours07/Script/LeantTweenValue.cs
@@ -11,11 +11,11 @@ public class LeantTweenValue : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        LeanTween.value(gameObject, 1, 0, _delay).setOnUpdate(UpdateLigthIntenisty).setOnComplete(ToggleLight);
+        LeanTween.value(gameObject, 1, 0, _delay).setOnUpdate(UpdateLigthIntensity).setOnComplete(ToggleLight);
 
     }
 
-    private void UpdateLigthIntenisty(float lightIntensity)
+    private void UpdateLigthIntensity(float lightIntensity)
     {
         _lightSource.intensity = lightIntensity;
     }
@@ -25,11 +25,11 @@ public class LeantTweenValue : MonoBehaviour
     {
         if(_lightSource.intensity==1.0f)
         {
-            LeanTween.value(gameObject, 1, 0, _delay).setEaseOutQuad().setOnUpdate(UpdateLigthIntenisty).setOnComplete(ToggleLight);
+            LeanTween.value(gameObject, 1, 0, _delay).setEaseOutQuad().setOnUpdate(UpdateLigthIntensity).setOnComplete(ToggleLight);
         }
         else
         {
-            LeanTween.value(gameObject, 0, 1, _delay).setEaseOutQuad().setOnUpdate(UpdateLigthIntenisty).setOnComplete(ToggleLight);
+            LeanTween.value(gameObject, 0, 1, _delay).setEaseOutQuad().setOnUpdate(UpdateLigthIntensity).setOnComplete(ToggleLight);
         }
     }
 }
