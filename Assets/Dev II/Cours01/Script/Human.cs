ecb64b7e47a8a326a19acb318b4687a1c8906791
Humarcoux <humarcoux@osullivan-quebec.qc.ca>
2024-01-15 15:11:16 -0500

    small update

--- Assets/Dev II/Cours01/Script/Human.cs
+++ Assets/Dev II/Cours01/Script/Human.cs
@@ -8,6 +8,7 @@ public class Human : MonoBehaviour
 
     public void EatAFruit()
     {
+        Debug.Log($"Human has eaten {_fruitList[0].name}");
         _vitamineCContent += _fruitList[0].GetEaten();
         Destroy(_fruitList[0].gameObject);
         _fruitList.RemoveAt(0);
