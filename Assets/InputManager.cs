c753fbae1c30b52946338ac0d585fd64d9ac9e02
Humarcoux <humarcoux@osullivan-quebec.qc.ca>
2024-03-15 13:02:03 -0400

    update

---
+++ Assets/InputManager.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    //[InputAxis]
+    //[SerializeField] private string _fireInput;
+
+    //[Dropdown("intValues")]
+    //public int intValue;
+
+    [SerializeField] private List<Ressource> _ressourceCost;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetButton(_fireInput))
+        //{
+        //  print("Fire");
+        // }
+    }
+}
---
+++ Assets/Ressource.cs
@@ -0,0 +1,15 @@
+using NaughtyAttributes;
+using System;
+using System.Collections.Generic;
+
+[Serializable]
+public class Ressource
+{
+
+    [Dropdown("_allType")]
+    public string _type;
+    public int _value;
+
+    private List<string> _allType { get { return new List<string>() { "Wood", "Mineral", "Gold", "Food" }; } }
+
+}
