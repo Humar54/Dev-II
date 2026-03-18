9af463dc14c52f137be5773f677f873999a9ba52
Hugo Marcoux <hugo.marcoux.oneiricgames@gmail.com>
2026-03-17 23:31:35 -0400

    update exe20

--- Assets/Dev II/Cours13/Script/Tree.cs
+++ Assets/Dev II/Cours13/Script/Tree.cs
@@ -1,35 +1,13 @@
 
 using NaughtyAttributes;
 using System;
-using UnityEngine;
+
 
 [Serializable]
 public class Tree
 {
-    [AllowNesting]
-    [MinValue(2f), MaxValue(10f)]
-    public float _lengthOfBranch; 
-    [MinValue(0), MaxValue(10)]
-    public int _numberOfBranch;
-    
-
+    [MinValue(0.2f), MaxValue(2f)]
     public float _radius;
+    [MinValue(0.2f), MaxValue(5f)]
     public float _height;
-
- 
-    public bool _hasLeaf;
-    [AllowNesting]
-    [ShowIf("_hasLeaf")]
-
-    public float _sizeOfLeaf;
-    [AllowNesting]
-    [ShowIf("_hasLeaf")]
-
-    public int _numberOfLeaf;
-    [AllowNesting]
-    [ShowIf("_hasLeaf")]
- 
-    public Color _colorOfLeaf;
-
-
 }
--- Assets/ForestGenerator.cs
+++ Assets/ForestGenerator.cs
@@ -1,58 +1,94 @@
 using NaughtyAttributes;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class ForestGenerator : MonoBehaviour
 {
+
+    [Dropdown("GetVectorValues")]
+    public Vector3 vectorValue;
+
+    private DropdownList<Vector3> GetVectorValues()
+    {
+        return new DropdownList<Vector3>()
+        {
+            { "Left",    Vector3.left },
+            { "Up",      Vector3.up },
+            { "Down",    Vector3.down },
+            { "Forward", Vector3.forward },
+            { "Back",    Vector3.back }
+        };
+    }
     public enum Shape
     {
-        Square,
+        None,
+        Rectangular,
         Circle,
     }
+
+    [SerializeField] private int _numberOfTreeToInstantiate;
 
     [Tooltip("List of all trees to be instantiated")]
     [SerializeField] private List<Tree> _treeList;
     [SerializeField] private GameObject _treePrefab;
     [SerializeField] private List<GameObject> _allTree;
 
-    [Foldout("Parameters")]
-    [Range(1, 10)]
-    [Tooltip("Range in which to instantiate Tree")]
-    [SerializeField] private float _forestRange = 5;
     
     [Foldout("Parameters")]
-    [AllowNesting]
+
     [SerializeField] private Shape _shape;
+
+
     [Foldout("Parameters")]
-    [AllowNesting]
+    [Tooltip("Radius in which to instantiate Tree")]
+    [ShowIf(nameof(IsCircle))]
+    [SerializeField] private float _radius = 5;
+
+
+    [Foldout("Parameters")]
+    [Tooltip("Rectangular zone in which to place a Tree")]
+    [ShowIf(nameof(IsRectangular))]
+    [SerializeField] private Vector2 _rectangleSize = new Vector2(1f,1f);
+
+
+    [Foldout("Parameters")]
     [SerializeField] private Vector3 _offset;
 
+
+
+    private bool IsCircle()      => (_shape == Shape.Circle);
+    private bool IsRectangular() => (_shape == Shape.Rectangular);
 
     [Button]
    
     public void AddTree()
     {
-        if(_shape==Shape.Square)
+        if(_shape==Shape.Rectangular)
         {
-            foreach (Tree tree in _treeList)
+            for (int i = 0; i < _numberOfTreeToInstantiate; i++)
             {
-                Vector3 randomPos = _offset + new Vector3(Random.RandomRange(-_forestRange, _forestRange), 0, Random.RandomRange(-_forestRange, _forestRange));
+                Vector3 randomPos = _offset + new Vector3(Random.RandomRange(-_rectangleSize.x/2f, _rectangleSize.x/2f), 0, Random.RandomRange(-_rectangleSize.y / 2f, _rectangleSize.y / 2f));
                 Quaternion randomRotation = Quaternion.Euler(new Vector3(0, Random.RandomRange(0, 360), 0));
                 GameObject newTree = Instantiate(_treePrefab, randomPos, randomRotation);
                 _allTree.Add(newTree);
-                newTree.transform.localScale = new Vector3(tree._radius, tree._radius, tree._height);
+
+                Tree currentTreeType = _treeList[Random.Range(0, _treeList.Count)];
+                newTree.transform.localScale = new Vector3(currentTreeType._radius, currentTreeType._height, currentTreeType._radius);
             }
+
         }
         if(_shape==Shape.Circle)
         {
             foreach (Tree tree in _treeList)
             {
-                Vector3 randomPos = Random.insideUnitSphere*_forestRange;
-                 randomPos = _offset + new Vector3(randomPos.x,0f, randomPos.z);
+                Vector3 randomPos = Random.insideUnitCircle*_radius;
+                 randomPos = _offset + new Vector3(randomPos.x,0f,randomPos.y);
                 Quaternion randomRotation = Quaternion.Euler(new Vector3(0, Random.RandomRange(0, 360), 0));
                 GameObject newTree = Instantiate(_treePrefab, randomPos, randomRotation);
                 _allTree.Add(newTree);
-                newTree.transform.localScale = new Vector3(tree._radius, tree._radius, tree._height);
+                Tree currentTreeType = _treeList[Random.Range(0, _treeList.Count)];
+                newTree.transform.localScale = new Vector3(currentTreeType._radius, currentTreeType._height, currentTreeType._radius);
             }
         }
 
@@ -66,4 +102,40 @@ public class ForestGenerator : MonoBehaviour
             _allTree.RemoveAt(i);
         }
     }
+
+    private void OnDrawGizmosSelected()
+    {
+        if (_shape == Shape.Rectangular)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawWireCube( _offset, new Vector3(_rectangleSize.x,1f,_rectangleSize.y));
+        }
+
+        if(_shape == Shape.Circle)
+        {
+
+            Gizmos.color = Color.red;
+            DrawCircle(_offset, _radius, 64);
+        }
+
+
+
+
+    }
+
+    private void DrawCircle(Vector3 center, float radius, int segments)
+    {
+        float angleStep = 360f / segments;
+        Vector3 prevPoint = center + new Vector3(Mathf.Cos(0f), 0f, Mathf.Sin(0f)) * radius;
+
+        for (int i = 1; i <= segments; i++)
+        {
+            float angle = angleStep * i * Mathf.Deg2Rad;
+            Vector3 nextPoint = center + new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * radius;
+
+            Gizmos.DrawLine(prevPoint, nextPoint);
+            prevPoint = nextPoint;
+        }
+    }
+
 }
